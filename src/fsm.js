ffb461db4adb248572f46917dfff5ebb5a6ac373
Marina Sugutova <marinasugutova@gmail.com>
2019-09-27 20:57:39 +0300

    feat: add part  task

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,53 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.config = config;
+        this.state = config.initial;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(this.config.states[state]){
+            this.state = state;        
+        }else{
+            throw new Error('state isn\'t exist');
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+
+        const nextState = this.config.states[this.states].transitions[event];
+        
+
+       if (this.state && nextState){
+           this.state = nextState;
+       }else{
+            throw new Error('Error');
+       }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
